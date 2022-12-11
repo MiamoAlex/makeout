f17ff869ec3148985e17a1dea3613307ffaa0333
Sounalet Alexandre <alex.sounalet@hotmail.fr>
2022-12-11 14:17:49 +0100

    Formattage de partial

--- public/makeout/View/UiRenderer.js
+++ public/makeout/View/UiRenderer.js
@@ -2,12 +2,17 @@
  * 👁️ UiRenderer s'occupe du rendu de la vue de l'application en passant du templating par la traduction des données à afficher
  */
 export class UiRenderer {
-    constructor() {
-
-    }
-
     // Elements visuels de l'application
     domElements = {};
+
+    templates = ['message'];
+    templatesElements = {};
+
+    constructor() {
+        for (let i = 0; i < this.templates.length; i++) {
+            this.templatesElements[this.templates[i]] = document.querySelector(`.template__${this.templates[i]}`);
+        }
+    }
 
     /**
      * appendDomElements() ajoute aux elements visuels actuels un groupe de nouveau éléments récupérables
@@ -17,7 +22,6 @@ export class UiRenderer {
         for (const key in domElements) {
             this.domElements[key] = document.querySelector(domElements[key].element);
         }
-        console.log(this.domElements);
     }
 
     /**
@@ -40,5 +44,30 @@ export class UiRenderer {
         const section = this.getElement('mainCore').children[layoutId];
         section.className = `main__section ${partialName}`;
         section.innerHTML = partialContent;
+    }
+
+    /**
+     * renderTemplate() formatte une template à partir d'un tableau d'objet et l'envoie dans le dom destination
+     * @param {Node} template 
+     * @param {Array<Object>} arrayObj 
+     * @param {Node} destination 
+     */
+    renderTemplate(template, arrayObj, destination) {
+        const toFormat = Array.from(template.innerHTML.matchAll(/{{(.*?)}}/gi));
+        let formattedTemplates = '';
+        for (let i = 0; i < arrayObj.length; i++) {
+            const obj = arrayObj[i];
+            formattedTemplates += template.innerHTML;
+            for (let j = 0; j < toFormat.length; j++) {
+                const tag = toFormat[j][0];
+                const key = toFormat[j][1];
+                if (obj[key]) {
+                    formattedTemplates = formattedTemplates.replaceAll(tag, obj[key]);
+                } else {
+                    formattedTemplates = formattedTemplates.replaceAll(tag, '');
+                }
+            }
+        }
+        // destination.innerHTML = formattedTemplates;
     }
 }
