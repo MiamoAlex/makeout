95ce39d548e874ccbbaf135dd7fa228a9c44a198
xernois <antoine2496@gmail.com>
2023-01-03 22:25:32 +0100

    fix typo route

--- api/routes/lover.route.ts
+++ api/routes/lover.route.ts
@@ -3,9 +3,9 @@ const router = express.Router();
 import { getLovers, rejectLovers, acceptLovers, editLover } from "../controllers/lover.controller";
 import { checkTokenMiddleware } from "../middlewares/auth.middleware"; 
 
-router.get("/getLovers", checkTokenMiddleware, getLovers);
-router.put("/rejectLovers/:id", checkTokenMiddleware, rejectLovers);
-router.put("/acceptLovers/:id", checkTokenMiddleware, acceptLovers);
+router.get("/getlovers", checkTokenMiddleware, getLovers);
+router.put("/rejectlovers/:id", checkTokenMiddleware, rejectLovers);
+router.put("/acceptlovers/:id", checkTokenMiddleware, acceptLovers);
 router.put("/editprofile", checkTokenMiddleware, editLover);
 
 export default router;
